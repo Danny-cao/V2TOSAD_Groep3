e019e7f3c40c5350c6fdde3f2b20c85f40f73725
Ricardo Tillemans <ricardotillemans@outlook.com>
2019-01-25 12:05:49 +0100

    init

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,8 @@
 import transform.model.Attribute_Range;
 import transform.model.BusinessRule;
 import transform.model.BusinessRuleType;
+import transform.persistence.BusinessRuleDao;
+import transform.persistence.BusinessRuleOracleDaoImpl;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,6 +12,8 @@ public class Main {
         BusinessRuleType brt1 = new BusinessRuleType(1, "Type1", "Categorie1");
         Attribute_Range c1 = new Attribute_Range("Test", "Table", 1, "Attribute", "yes",1, 10);
         BusinessRule br1 = new BusinessRule(1, "Test", brt1, c1);
+
+        BusinessRuleDao dao = new BusinessRuleOracleDaoImpl();
 
         System.out.println(((Attribute_Range) br1.getConstraint()).getBetween());
 
